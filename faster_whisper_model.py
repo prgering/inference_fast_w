5891b234994de77dbd0605d8fae5d71367aef655
prgering <prgering1@sheffield.ac.uk>
2024-01-09 14:53:12 +0000

    Another commit

--- faster_whisper_model.py
+++ faster_whisper_model.py
@@ -1,7 +1,7 @@
 from faster_whisper import WhisperModel
 import sys
 model = WhisperModel("base.en")
-#segments, info = model.transcribe("1702901102917_scenariov3_035.wav")
+
 segments, info = model.transcribe(sys.argv[1])
 
 for segment in segments:
